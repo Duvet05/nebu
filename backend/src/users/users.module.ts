1e24b4b9d5f622d3fc708f925370d8c9e7616f77
duvet05 <gonzalo.galvezc@pucp.edu.pe>
2025-11-28 04:24:51 +0100

    feat: agregar entidades Person, Privilege y Role al módulo de usuarios

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { User } from './entities/user.entity';
+import { Person } from './entities/person.entity';
+import { Privilege } from './entities/privilege.entity';
+import { Role } from './entities/role.entity';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Person, Privilege, Role])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
