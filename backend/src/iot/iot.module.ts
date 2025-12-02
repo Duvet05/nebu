3b9e4ec87879d395a9c98f7705bbe3d24435380f
duvet05 <gonzalo.galvezc@pucp.edu.pe>
2025-12-02 17:24:24 +0100

    ..

--- backend/src/iot/iot.module.ts
+++ backend/src/iot/iot.module.ts
@@ -5,11 +5,13 @@ import { IoTController } from './iot.controller';
 import { DeviceTokenController } from './device-token.controller';
 import { ESP32TokenController } from './esp32-token.controller';
 import { IoTDevice } from './entities/iot-device.entity';
+import { DeviceModel } from './entities/device-model.entity';
+import { FirmwareVersion } from './entities/firmware-version.entity';
 import { LiveKitModule } from '../livekit/livekit.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([IoTDevice]),
+    TypeOrmModule.forFeature([IoTDevice, DeviceModel, FirmwareVersion]),
     LiveKitModule,
     // JwtModule is already globally configured in app.module.ts
   ],
