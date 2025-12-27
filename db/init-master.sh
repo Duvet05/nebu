0dea21670920a3183734122cf80cf9be8cfc2930
duvet05 <gonzalo.galvezc@pucp.edu.pe>
2025-12-27 01:08:21 +0100

    feat: actualizar script de inicialización de PostgreSQL para soporte de replicación y agregar configuración en docker-compose

--- db/init-master.sh
+++ db/init-master.sh
@@ -8,30 +8,25 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 DO
 \$\$
 BEGIN
-    IF '${OMRS_DB_REPL_USER}' != '' AND '${OMRS_DB_REPL_PASSWORD}' != '' THEN
-        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${OMRS_DB_REPL_USER}') THEN
-            CREATE ROLE ${OMRS_DB_REPL_USER} LOGIN PASSWORD '${OMRS_DB_REPL_PASSWORD}';
-            RAISE NOTICE 'Replication user created.';
+    IF '${REPLICATION_USER}' != '' AND '${REPLICATION_PASSWORD}' != '' THEN
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${REPLICATION_USER}') THEN
+            CREATE ROLE ${REPLICATION_USER} WITH REPLICATION LOGIN PASSWORD '${REPLICATION_PASSWORD}';
+            RAISE NOTICE 'Replication user created with replication privileges.';
         ELSE
             RAISE NOTICE 'Replication user already exists.';
-        END IF;
-    END IF;
-END
-\$\$;
-
-DO
-\$\$
-BEGIN
-    IF '${OMRS_DB_BACKUP_USER}' != '' AND '${OMRS_DB_BACKUP_PASSWORD}' != '' THEN
-        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '${OMRS_DB_BACKUP_USER}') THEN
-            CREATE ROLE ${OMRS_DB_BACKUP_USER} LOGIN PASSWORD '${OMRS_DB_BACKUP_PASSWORD}';
-            RAISE NOTICE 'Backup user created.';
-        ELSE
-            RAISE NOTICE 'Backup user already exists.';
+            -- Asegurar que tenga privilegios de replicación
+            ALTER ROLE ${REPLICATION_USER} WITH REPLICATION;
         END IF;
     END IF;
 END
 \$\$;
 EOSQL
+
+# Configurar pg_hba.conf para permitir replicación
+echo "host replication ${REPLICATION_USER} all md5" >> "$PGDATA/pg_hba.conf"
+echo "host all all all md5" >> "$PGDATA/pg_hba.conf"
+
+# Recargar configuración
+pg_ctl reload -D "$PGDATA" || true
 
 echo "Master DB initialization complete."
